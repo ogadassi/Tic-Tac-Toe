e183e61600ce896b5dd227c0557c458ca65e9141
ogadassi <ogadassi@gmail.com>
2024-05-08 14:12:58 +0300

    live3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './components/Layout-area/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: LayoutComponent,
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
